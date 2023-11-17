1e733ddb74975ec1c7058f0cb8973dab1376e2bd
ajiq <haziqruslan27@gmail.com>
2023-11-18 04:41:18 +0800

    justgo

--- App.js
+++ App.js
@@ -3,10 +3,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import DashboardScreen from './screens/DashboardScreen';
+import Geocoder from 'react-native-geocoding';
 import ForgetPassword from './screens/ForgetPassword';
 import LoginScreen from './screens/LoginScreen';
+import PassengerMenu from './screens/PassengerMenu';
 import SignupScreen from './screens/SignupScreen';
+
+Geocoder.init('AIzaSyCeZnCGy1kggLJnYpVBjrms39JD9SBjlQ0');
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +19,7 @@ function App() {
       <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Dashboard" component={PassengerMenu} />
         <Stack.Screen name="ForgetPass" component={ForgetPassword} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ screens/PassengerMenu.js
@@ -0,0 +1,238 @@
+import { useNavigation } from '@react-navigation/native';
+import * as Location from 'expo-location';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { Image, KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import Geocoder from 'react-native-geocoding';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapView, { Callout, Marker } from 'react-native-maps';
+
+Geocoder.init('AIzaSyCeZnCGy1kggLJnYpVBjrms39JD9SBjlQ0');
+
+export default function PassengerMenu() {
+    const navigation = useNavigation();
+    const [searchText, setSearchText] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const [pin, setPin] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324
+    })
+    const [region, setRegion] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    })
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    const handleSelectLocation = (location) => {
+        // Handle the selected location
+        console.log('Selected Location:', location);
+    };
+
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <StatusBar backgroundColor="black" style='light' />
+            <Image style={styles.backgroundImage} source={require('../assets/images/background.png')} />
+
+            {/* logo */}
+            <View style={styles.headerContainer}>
+                <View style={styles.logoContainer}>
+                    <Image
+                        source={require('../assets/images/justgoHeader.png')} // Update the path to your image
+                        style={styles.logo}
+                        resizeMode="contain"
+                    />
+                </View>
+            </View>
+
+            <View style={styles.formContainer}>
+                {/* search bar */}
+                    <GooglePlacesAutocomplete
+                        placeholder="Search"
+                        fetchDetails={true}
+                        GooglePlacesSearchQuery={{
+                            rankby: "distance"
+                        }}
+                        onPress={(data, details = null) => {
+                            // 'details' is provided when fetchDetails = true
+                            console.log(data, details)
+                            setRegion({
+                                latitude: details.geometry.location.lat,
+                                longitude: details.geometry.location.lng,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421
+                            })
+                        }}
+                        query={{
+                            key: "AIzaSyCeZnCGy1kggLJnYpVBjrms39JD9SBjlQ0",
+                            language: "en",
+                            components: "country:my",
+                            types: "establishment",
+                            radius: 1000,
+                            location: `${region.latitude}, ${region.longitude}`
+                        }}
+                        styles={{
+                            container: { width: '80%', flex: 0, position: "absolute", zIndex: 1 },
+                            listView: { backgroundColor: "white", zIndex: 1 }
+                        }}
+                    />
+
+                <View style={styles.mapContainer}>
+                    <MapView
+                        style={styles.map}
+                        region={{
+                            latitude: region.latitude,
+                            longitude: region.longitude,
+                            latitudeDelta: 0.001,
+                            longitudeDelta: 0.001,
+                        }}
+                        provider="google"
+                    >
+                        <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
+                        <Marker
+                            coordinate={pin}
+                            pinColor="red"
+                        >
+                            <Callout>
+                                <Text>I'm here</Text>
+                            </Callout>
+                        </Marker>
+                    </MapView>
+                </View>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        backgroundColor: 'maroon', // Set the background color of the header
+        padding: 5,
+        justifyContent: 'space-between',
+        flexDirection: 'row', // Arrange children in a row
+        marginTop: 40,
+        borderBottomLeftRadius: 20, // Bottom left corner radius
+        borderBottomRightRadius: 20, // Bottom right corner radius
+    },
+    logo: {
+        width: 150, // Set the width of your logo
+        height: 50, // Set the height of your logo
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    backgroundImage: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    logoContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginRight: 10,
+    },
+    logoImage: {
+        width: 300,
+        height: 300,
+    },
+    formContainer: {
+        flex: 1,
+        justifyContent: 'top',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    inputContainer: {
+        width: '80%',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        marginBottom: 20,
+        padding: 10,
+        elevation: 3,
+        shadowColor: 'rgba(0,0,0,0.2)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+    },
+    input: {
+        color: 'maroon',
+    },
+    buttonContainer: {
+        width: '80%',
+        marginBottom: 20,
+    },
+    button: {
+        width: '100%',
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 20,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    linkContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    linkText: {
+        color: 'white',
+        textDecorationLine: 'underline',
+        marginLeft: 5,
+    },
+    searchContainer: {
+        width: '80%',
+        backgroundColor: 'transparent',
+        borderBottomWidth: 0,
+        borderTopWidth: 0,
+        marginBottom: 20,
+    },
+    searchInputContainer: {
+        backgroundColor: 'white',
+        borderRadius: 20,
+        borderBottomWidth: 0, // Remove the default border
+    },
+    searchInput: {
+        color: 'maroon',
+    },
+    searchResultsContainer: {
+        marginTop: 10,
+        width: '80%',
+    },
+    searchResultItem: {
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray',
+    },
+    searchResultText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    map: {
+        width: 350,
+        height: 500,
+    },
+    mapContainer: {
+        flex: 1,
+        paddingTop: 70,
+    },
+});
